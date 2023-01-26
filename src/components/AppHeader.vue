34c148f1bfde7bc93a0e23f2cfc2b49a979c3f15
AbidemiT <tiamiyusikiruabidemi@gmail.com>
2023-01-26 12:19:39 +0100

    routing

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,21 +3,29 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" :to="{ name: 'home' }"
+        exact-active-class="no-active">Music</router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <router-link class="px-2 text-white" :to="{ name: 'about' }">
+            About
+          </router-link>
           <li v-if="!userLoggedIn">
-            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">Login / Register</a>
+            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">
+              Login / Register
+            </a>
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#" @click.prevent="logOut">LogOut</a>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
             </li>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <a class="px-2 text-white" href="#" @click.prevent="logOut">
+                LogOut
+              </a>
             </li>
           </template>
         </ul>
@@ -45,6 +53,11 @@ export default {
     async logOut() {
       try {
         await this.signOut();
+
+        // console.log({ route: this.$route });
+        if (this.$route.meta.requiresAuth) {
+          this.$router.push({ name: "home" });
+        }
       } catch (error) {
         console.log({ error });
       }
